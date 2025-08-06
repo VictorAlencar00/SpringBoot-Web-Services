9f74252088f8e3429e3092e54a93604b0fb16996
VictorAlencar00 <victordesousaa@gmail.com>
2025-08-05 22:26:20 -0300

    feat: service layer add

---
+++ src/main/java/com/github/VictorAlencar00/web_services/services/UserService.java
@@ -0,0 +1,28 @@
+package com.github.VictorAlencar00.web_services.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.VictorAlencar00.web_services.entities.User;
+import com.github.VictorAlencar00.web_services.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
