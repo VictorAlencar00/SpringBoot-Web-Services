f37445c48a557380844fb5d60a0de10a3d7ddf92
VictorAlencar00 <victordesousaa@gmail.com>
2025-08-12 20:41:03 -0300

    feat: Product-OrderItem OneToMany association

--- src/main/java/com/github/VictorAlencar00/web_services/entities/OrderItem.java
+++ src/main/java/com/github/VictorAlencar00/web_services/entities/OrderItem.java
@@ -36,7 +36,7 @@ public class OrderItem {
         id.setOrder(order);
     }
 
-    public Product getProduct(Product product) {
+    public Product getProduct() {
         return id.getProduct();
     }
 
