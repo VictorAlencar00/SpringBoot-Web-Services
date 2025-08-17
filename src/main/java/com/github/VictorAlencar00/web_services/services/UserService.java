393d772cc2d35128087cd1d49c22c15f3ca37dce
VictorAlencar00 <victordesousaa@gmail.com>
2025-08-17 18:31:33 -0300

    feat: User update exception handling add

--- src/main/java/com/github/VictorAlencar00/web_services/services/UserService.java
+++ src/main/java/com/github/VictorAlencar00/web_services/services/UserService.java
@@ -13,6 +13,8 @@ import com.github.VictorAlencar00.web_services.repositories.UserRepository;
 import com.github.VictorAlencar00.web_services.services.exceptions.DatabaseException;
 import com.github.VictorAlencar00.web_services.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -47,9 +49,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
